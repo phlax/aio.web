976f9bd08ec87a538a23f8c1a4bb1568be30762e
Ryan Northey <ryan@space23.net>
2015-05-16 16:18:14 +0000

    removing debugging

--- aio/web/__init__.py
+++ aio/web/__init__.py
@@ -32,8 +32,6 @@ def setup_templates(app):
     templates = []
     for module in aio.app.modules:
         templates.append(os.path.join(module.__path__[0], "templates"))
-    if templates:
-        print(os.listdir(templates[0]))
     aiohttp_jinja2.setup(
         app,
         loader=jinja2.FileSystemLoader(templates))
