19df51f7ce30c8adb339edb0a155afd72d21cbd7
Ryan Northey <ryan@space23.net>
2015-05-18 22:50:36 +0000

    adding long_description

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 """
 aio.web
 """
+import os
 import sys
 from setuptools import setup, find_packages
 
@@ -20,12 +21,34 @@ if sys.version_info < (3, 4):
 
 tests_require = install_requires + ['aio.testing']
 
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description = (
+    'Detailed documentation\n'
+    + '**********************\n'
+    + '\n'
+    + read("README.rst")
+    + '\n')
+
+try:
+    long_description += (
+        '\n'
+        + read("aio", "http", "README.rst")
+        + '\n')    
+except FileNotFoundError:
+    pass
+
+
 setup(
     name='aio.web',
     version=version,
     description="Aio web server",
+    long_description=long_description,
     classifiers=[
-        "Programming Language :: Python 3.4",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.4",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
     keywords='',
